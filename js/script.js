868c425e88be08bd3c4a5f5cb5effa04c66f7872
Dexi <109952800+Betka-H@users.noreply.github.com>
2023-07-19 11:56:58 +0200

    more header n footer

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* let ans=["yep","sure","mayb","8","absolutely not"];
+
+function ask8b(){
+    let que=prompt("ask the magic 8 ball");
+}
+
+if que={
+    let randomin=
+    Math.floor(Math.random()*ans.length);
+}
+
+alert("magic 8 says:"+ans[randomin]); */
